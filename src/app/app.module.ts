32c2c8c0cffc8b480c17f7a43e565281a1ce510a
PaoloCarugati <paolocarugati76@gmail.com>
2023-05-04 12:58:42 +0000

    primo commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AnimalsComponent } from './animals/animals.component';
+import { FruitsComponent } from './fruits/fruits.component';
+import { GenericComponent } from './generic/generic.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AnimalsComponent,
+    FruitsComponent,
+    GenericComponent
   ],
   imports: [
     BrowserModule,
